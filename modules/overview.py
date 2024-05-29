ded29ef762164c47aa15a3c8bb198940561b85b1
fatualux <35587292+fatualux@users.noreply.github.com>
2024-05-29 11:39:46 +0200

    Fixed formatting

--- modules/overview.py
+++ modules/overview.py
@@ -59,36 +59,25 @@ class Overview:
             overview_str += (
                 f"Last Update Time: {self.escape_markdown(overview.get('lastUpdateTime'))}\n"
                 "Life Time Data:\n"
-                "  Energy: {:.3f} kWh\n".format(energy_life_time_kwh) +
+                f"  Energy: {energy_life_time_kwh:.3f} kWh\n" +
                 f"  Revenue: {self.escape_markdown(str(overview['lifeTimeData']['revenue']))} €\n"
                 "Last Year Data:\n"
             )
             overview_str += (
-                "  Energy: {:.3f} kWh\n".format(energy_last_year_kwh)
-                if energy_last_year_kwh else "  No data available for last year.\n"
+                f"  Energy: {energy_last_year_kwh:.3f} kWh\n" if energy_last_year_kwh else "  No data available for last year.\n"
             )
             overview_str += "Last Month Data:\n"
             overview_str += (
-                "  Energy: {:.3f} kWh\n".format(energy_last_month_kwh)
-                if energy_last_month_kwh else "  No data available for last month.\n"
+                f"  Energy: {energy_last_month_kwh:.3f} kWh\n" if energy_last_month_kwh else "  No data available for last month.\n"
             )
             overview_str += "Last Day Data:\n"
             overview_str += (
-                "  Energy: {:.3f} kWh\n".format(energy_last_day_kwh)
-                if energy_last_day_kwh else "  No data available for last day.\n"
+                f"  Energy: {energy_last_day_kwh:.3f} kWh\n" if energy_last_day_kwh else "  No data available for last day.\n"
             )
             overview_str += (
                 "Current Power:\n"
-                f"  Power: {self.escape_markdown('{:.3f}'.format(power_kw))} kW\n"
+                f"  Power: {power_kw:.3f} kW\n"
             )
             return overview_str
         else:
             return "No site overview data found."
-
-
-# Example usage
-token = "your_api_key"
-site_id = "your_site_id"
-overview = Overview(token)
-data = overview.get_site_overview(site_id)
-print(overview.print_site_overview(data))
